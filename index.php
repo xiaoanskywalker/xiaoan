cd52c5dbc107296127d46307c46814ef7dfa562b
xasq <zqvvvv@126.com>
2017-02-11 19:56:15 +0800

    test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ if((file_exists("./common/config.php"))==false){
     header("location:./install/");
 }
 session_start();
-/*帖子分页预处理1*/
+/*帖子分页预处理31*/
 $page=@$_REQUEST["page"];
 if($page==null){$page=1;}
 is_numeric($page) or die("<script> alert('无效的参数!');window.navigate('./');</script>");
