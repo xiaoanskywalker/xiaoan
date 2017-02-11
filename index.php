0e8a41ff802c78b91f997df88b606d53985691ca
Gogo <gogo81745.moe@gmail.com>
2017-02-11 21:50:07 +0800

    主页修改

--- index.php
+++ index.php
@@ -1,164 +1,236 @@
 <?php
-if((file_exists("./common/config.php"))==false){
+if ((file_exists("./common/config.php")) == false) {
     header("location:./install/");
 }
 session_start();
 /*帖子分页预处理31*/
-$page=@$_REQUEST["page"];
-if($page==null){$page=1;}
+$page = @$_REQUEST["page"];
+if ($page == null) {
+    $page = 1;
+}
 is_numeric($page) or die("<script> alert('无效的参数!');window.navigate('./');</script>");
-if($page<=0){
-    $page=1;
+if ($page <= 0) {
+    $page = 1;
     die("<script> alert('page参数非正整数!');window.navigate('./');</script>");
 }
-$pagestart=round(($page-1)*40);
-$pageend=round($page*40+1);
+$pagestart = round(($page - 1) * 40);
+$pageend = round($page * 40 + 1);
+
+
+/**
+ * 未实现内容
+ * 获取每个发帖用户的头像
+ * 获取每个帖子评论数量
+ */
+
+
+/**
+ * 站点信息
+ */
+
+require_once './common/config.php';
+require_once './common/conn.php';
+$rs = mysql_query("SELECT * FROM wtb_general_settings where gid=1") or die("连接到数据库时出现错误。");
+$row = mysql_fetch_row($rs);
+
+$site_info = array('title' => $row[1], 'keywords' => $row[2], 'description' => $row[3]);
+
+/**
+ * 用户
+ */
+
+$user = array('name' => $_SESSION["user"]);
+
+if ($user['name'] != null) {
+    $file = "./common/images/avatar/" . $user['name'] . ".png";
+    if (file_exists($file)) {
+        $user['avatar'] = $file;
+    } else {
+        $user['avatar'] = './static/img/avatar.jpg';
+    }
+} else {
+    $user['avatar'] = './static/img/avatar.jpg';
+}
+
+/**
+ * 帖子
+ */
+$discussions = array();
+
+$rs = mysql_query("SELECT * FROM wtb_titles ORDER BY tid DESC limit $pagestart,$pageend");
+while ($row = mysql_fetch_row($rs)) {
+    $discussion = array();
+    $discussion['tid'] = $row[0];
+    $discussion['author'] = $row[1];
+    $discussion['title'] = $row[2];
+    $discussion['time'] = $row[3];
+    $discussion['preview'] = $row[4];
+
+    $temp = explode("-", $row[3]);
+    $discussion['time'] = date('mm月dd日', mktime(0, 0, 0, $temp[1], $temp[2], $temp[0]));
+
+
+    array_push($discussions, $discussion);
+}
+
+/**
+ * 页码
+ */
+$pagination = array();
+
+for ($i = -5; $i <= 5; $i++) {
+    $id = $page + $i;
+    if ($id >= 1) {
+        $temp = array('id' => $id, 'url' => './?page=' . $id);
+        array_push($pagination, $temp);
+    }
+}
 ?>
+
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="./common/css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="./common/css/style.css" />
-        <style type="text/css">
-         body {background-image:url(./common/images/bg_<?php echo(rand(1,3));?>.jpg);}
-        </style>
-        <?php
-        require_once './common/config.php';
-        require_once './common/conn.php';
-        $rs=mysql_query("SELECT * FROM wtb_general_settings where gid=1") or die("连接到数据库时出现错误。");
-        $row = mysql_fetch_row($rs);
-        echo "<title>$row[1]</title><meta name='keywords' content='$row[2]' /><meta name='description' content='$row[3]' />" ;
-        ?>
-         <script language="javascript"> 
-         function keypress1() //发帖标题输入长度处理 
-         { 
-         var text1=document.getElementById("mytext1").value; 
-         var len=50-text1.length; 
-         var show="你还可以输入"+len+"个字"; 
-         document.getElementById("name").innerText=show; 
-         } 
-         function keypress2() //发帖内容输入长度处理 
-         { 
-         var text1=document.getElementById("myarea").value; 
-         var len;//记录剩余字符串的长度 
-         if(text1.length>=10000)//textarea控件不能用maxlength属性，就通过这样显示输入字符数了 
-         { 
-         document.getElementById("myarea").value=text1.substr(0,300); 
-         len=0; 
-         } 
-         else 
-         { 
-         len=10000-text1.length; 
-         } 
-         var show="你还可以输入"+len+"个字"; 
-         document.getElementById("pinglun").innerText=show; 
-         } 
-         </script> 
-    </head>
-    <body>
-    <div id="index">
-         <!--
-        <div id="logo">
-            <div style="width:65%; float:left;">
-                <p><img src="images/logo.png" />
-            </div>
-           -->
-            <div id="right">
-                <div id="avatar">
-                   <a href="#" ><img src="./common/images/avatar/<?php
-                   if($_SESSION["user"]==null){echo "default_avatar";}
-                   else{
-                        $file = "./common/images/avatar/".$_SESSION["user"].".png";
-                        if(file_exists($file)){echo $_SESSION["user"];}
-                        else{echo "default_avatar";}
-                   }
-                   ?>.png"  height="100" width ="100" /></a> 
+<html lang="zh-cn">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <?php
+    echo "<title>$site_info[title]</title>";
+    echo "<meta name='keywords' content='$site_info[keywords]' />";
+    echo "<meta name='description' content='$site_info[description]' />";
+    ?>
+    <link href="./static/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./static/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="./static/css/style.css">
+
+</head>
+<body>
+
+<div class="layout">
+    <div class="top-bar">
+        <nav class="navbar">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">小安社区</a>
                 </div>
-                <div id="loginbtn">
-                    <?php
-                    if($_SESSION["user"]==null){
-                        echo "登录后内容更精彩<p>";
-                        echo"<button onclick='login()' class=lgbtn>登录</button>
-                    <button onclick='register()' class=regbtn>注册</button>";
-                    }
-                    else{
-                        echo ($_SESSION["user"])."<p>";
-                        echo "<button onclick='logout()') class=lgbtn>退出登录</button>";
-                    }
-                    ?>
+
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <?php
+                        if ($user['name'] == null) {
+                            ?>
+                            <li><a href="./user/login.php">登录</a></li>
+                            <li><a href="./user/register.php">注册</a></li>
+                            <?php
+                        } else {
+                            ?>
+                            <li class="dropdown user-nav-dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">
+                                    <span><?= $user['name'] ?></span>
+                                    <span class="caret"></span>
+                                </a>
+                                <ul class="dropdown-menu user-nav">
+                                    <li class="user-profile">
+                                        <img src="<?= $user['avatar'] ?>" class="avatar">
+                                        <span class="username"><?= $user['name'] ?></span>
+                                    </li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="#">我的空间</a></li>
+                                </ul>
+                            </li>
+                            <?php
+                        }
+                        ?>
+                    </ul>
                 </div>
             </div>
-        <div id="main">
-            <div id="search">
-                <ol class="breadcrumb">
-                <li><a href="#"><a href="./">首页</a></li>
-                </ol>
-            </div>
-            <div id="show">
-                <?php
-                 $rs = mysql_query("SELECT * FROM wtb_titles ORDER BY tid DESC limit $pagestart,$pageend");
-                 while($row = mysql_fetch_row($rs)) {
-                 echo "<div id='showtopic'>";
-                     /*帖子标题*/
-                     echo "<div id='showtopictitle'>";
-                     echo "<a href='showtopic.php?tid=$row[0]' target='_blank'>";
-                     echo(substr($row[2],0,46)."</a>");
-                     echo "</div>";
-                     /*帖子内容*/
-                     echo "<div id=’showtopicinclude'>";
-                     echo (substr($row[4],0,100)."...");
-                     echo "</div>";
-                     /*发帖作者及发布时间*/
-                     echo "<div id=’showtopicuser'>";
-                     echo($row[1]."发表于".$row[3]);
-                     echo "</div></div><p></p>";
-                 }
-                 /*显示页码*/
-                 if (mysql_num_rows($rs)<1){echo"记录集为空";}
-                 echo"<div id='page'>";       
-                 for($i=-5;$i<=5;$i++){
-                    $pge=$page+$i;
-                    if($pge>=1){
-                        echo ("<a href='./?page=$pge'>$pge</a>&nbsp;"); 
-                    } 
-                 }
-                ?>
-                <a href="./">首页</a>
-                <input type = "text" id="pagea" name="pagea" placeholder="输入页码..."></input>
-                <a href="#" onclick="pagego()">GO</a>
-                <script type="text/javascript">
-                    function pagego(){
-                    var aa = document.getElementById("pagea").value;
-                    window.navigate('./?page=' + aa);
-                    }
-                     function register(){
-                    self.location='./user/register.php'; 
-                    }
-                    function logout(){
-                    self.location='./user/logout.php'; 
-                    }
-                    function login(){
-                    self.location='./user/login.php'; 
-                    }     
-                </script><p>
-                <div id="addtopic">      
-                        <b>发表主题 New Topic</b>
-                        <form method="POST" action="./common/addtopic.php?action=newtopic">
-                        <input type=text name="title" id="mytext1" onKeyUp="keypress1()" placeholder="请输入标题...不能超过50个字" maxlength="50" size="80" id="addtopictitle">
-                        <font color="gray"><label id="name">你还可以输入50个字</label></font> <p>
-                        <textarea rows="15" id="myarea" onKeyUp="keypress2()" onblur="keypress2()" name="topic" cols="150" placeholder="请输入内容...不能超过10000个字" maxlength="10000" style="outline: none;">
-                        </textarea><p><input type="submit" value="发帖" name="ok">
-                        <font color="gray">&nbsp;&nbsp;<label id="pinglun">你还可以输入10000个字</label></font>
-                        </form>
-                </div>
-            </div>
-        </div>
+        </nav>
     </div>
-         </div>    
-    <hr><footer>
-    <div id="foot">&copy; <a rel="nofollow" href="http://xiaoanbbs.cn">小安社区</a>丨V0.2.0</div>
-    </footer>
+    <div class="container">
+
+        <header class="header jumbotron">
+            <p>小安社区，追求简单、极致</p>
+        </header>
+
+        <main class="main">
+            <aside class="sidebar">
+                <ul>
+                    <li><a href="#"><i class="zmdi zmdi-comments"></i>话题</a></li>
+                    <li><a href="#"><i class="zmdi zmdi-star"></i>关注</a></li>
+                    <li><a href="#"><i class="zmdi zmdi-view-list"></i>分类</a></li>
+                </ul>
+            </aside>
+            <div class="content">
+                <div class="discussion-list">
+                    <ul class="discussions">
+
+                        <?php
+                        foreach ($discussions as $discussion) {
+                            ?>
+                            <li>
+                                <div class="discussion">
+                                    <div class="avatar-area">
+                                        <?php
+                                        //TODO 获取每个发帖用户的头像
+                                        ?>
+                                        <a href="#"><img src="./static/img/avatar.jpg" class="avatar"
+                                                         alt="<?= $discussion['author'] ?>"></a>
+                                    </div>
+
+                                    <div class="profile">
+                                        <h3 class="title"><a href="#"><?= $discussion['title'] ?></a></h3>
+                                        <div class="preview">
+                                            <?= $discussion['preview'] ?>
+                                        </div>
+                                        <div class="info">
+                                            <a href="#" class="author"><?= $discussion['author'] ?></a>
+                                            <span class="time"><?= $discussion['time'] ?></span>
+                                        </div>
+                                    </div>
+
+                                    <?php
+                                    //TODO 获取每个帖子评论数量
+                                    ?>
+                                    <div class="count"><i class="zmdi zmdi-comments"></i>NaN</div>
+                                </div>
+                            </li>
+                            <?php
+                        }
+                        ?>
+                    </ul>
+                </div>
+                <div class="toorbar">
+                    <nav aria-label="Page navigation">
+                        <ul class="pagination">
+                            <li>
+                                <a href="#" aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                </a>
+                            </li>
+                            <?php
+                            foreach ($pagination as $temp) {
+                                ?>
+                                <li><a href="<?= $temp['url'] ?>"><?= $temp['id'] ?></a></li>
+                                <?php
+                            }
+                            ?>
+                            <li>
+                                <a href="#" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </main>
+    </div>
+</div>
+
+
+<script src="./static/js/jquery-2.2.4.min.js"></script>
+<script src="./static/js/bootstrap.min.js"></script>
+
 </body>
-</html><p>
+</html>
+
