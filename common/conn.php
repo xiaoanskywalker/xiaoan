53d3bdea18dbb3d6b4e1bf99c5e25967efa45d89
xasq <zqvvvv@126.com>
2017-02-12 18:40:39 +0800

    conn.php mysql->mysqli

--- common/conn.php
+++ common/conn.php
@@ -1,7 +1,6 @@
 <?php
-ini_set('date.timezone','Asia/Shanghai');
-mysql_connect(constant("mysql_servername") ,constant("mysql_username") , constant("mysql_password"));
-mysql_select_db(constant("mysql_database")); 
-mysql_query("SET NAMES 'utf8'");//数据库编码
-mysql_query("SET time_zone = '+8:00'")//数据库时区
+ini_set('date.timezone','Asia/Shanghai');//PHP时区
+$con=mysqli_connect(constant("mysql_servername"),constant("mysql_username"),constant("mysql_password"),constant("mysql_database"));
+mysqli_query($con,"SET NAMES 'utf8'");//数据库编码
+mysqli_query($con,"SET time_zone = '+8:00'")//数据库时区
 ?>
