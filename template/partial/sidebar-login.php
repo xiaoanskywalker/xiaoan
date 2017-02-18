bc15c0137390e956386af426545d126c52128327
Gogo <gogo81745.moe@gmail.com>
2017-02-18 17:07:40 +0800

    重写登录页面

---
+++ template/partial/sidebar-login.php
@@ -0,0 +1,6 @@
+<aside class="sidebar">
+    <ul>
+        <li><a href="<?= $baseurl ?>/user/login.php"><i class="zmdi zmdi-account-box"></i>登录</a></li>
+        <li><a href="#"><i class="zmdi zmdi-account-add"></i>注册</a></li>
+    </ul>
+</aside>
