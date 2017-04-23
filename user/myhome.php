0abb4e446d881bbe0b651ac7879be8079e7fbf87
xasq <zqvvvv@126.com>
2017-04-23 20:32:12 +0800

    myhome

--- user/myhome.php
+++ user/myhome.php
@@ -1,28 +1,31 @@
 <?php
 session_start();
-if (!$_SESSION["user"]) {
+/*用户*/
+if ($_SESSION["user"] != null) {
+    $user = $_SESSION["user"];
+}else{
     header("location:../");
 }
-
+if ((file_exists("../common/config.php")) == false) {
+    header("location:../install/");
+}
 require_once '../common/conn.php';
 require_once '../model/Site.php';
 require_once '../model/User.php';
 require_once '../model/Home.php';
 
 $site = Site::get();
+$page = array();
 
 
 $baseurl = '..';
 $body = 'myhome.partial.php';
 
+$page = array();
+
 $page['body'] = array();
-$page['body']['class'] = 'login';
+$page['body']['class'] = 'index';
 
 $page['header'] = array();
-$page['header']['title'] = '个人中心';
-
-$page['sidebar'] = array();
-$page['sidebar']['content'] = 'sidebar-login.php';
-
+$page['header']['title'] = $site->description;
 require '../template/layout.php';
-
