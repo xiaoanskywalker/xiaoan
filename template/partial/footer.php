ced542274f6dcc7d11ac42cee5742f527aeeb5e0
xasq <zqvvvv@126.com>
2017-04-23 18:54:51 +0800

    install

--- template/partial/footer.php
+++ template/partial/footer.php
@@ -3,7 +3,7 @@
         Powered By <a href="http://xiaoanbbs.cn" target="_blank">Xiaoanbbs</a>&nbsp;V0.3.2
         <p><?php
         $etime=microtime(true);//程序执行结束的时间
-        $total=$etime-$stime;   //计算差值
+        $total=$etime-$stime;   //计算差值1
         ?>
         页面执行时间：<?= round($total,3) ?>秒
     </div>
