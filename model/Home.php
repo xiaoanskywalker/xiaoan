801a88481600b6831bf1dddf66c68f25437cab02
xasq <zqvvvv@126.com>
2017-05-12 21:10:15 +0800

    show avatar

--- model/Home.php
+++ model/Home.php
@@ -2,16 +2,16 @@
 class Home{
 
     static function Upload($filename){
-        if($dir==null){
+        if($filename==null){
             return 0;
         }
         $show=array();
-        array_push($show,$_FILES['myFile']['name']);
-        array_push($show,$_FILES['myFile']['size']);
-        array_push($show,$_FILES['myFile']['type']);
-        array_push($show,$_FILES['myFile']['error']);
+        array_push($show,$_FILES['myFile']['name']);//上传时的文件名
+        array_push($show,$_FILES['myFile']['size']);//文件大小
+        array_push($show,$_FILES['myFile']['type']);//文件类型
+        array_push($show,$_FILES['myFile']['error']);//错误类型
         move_uploaded_file($_FILES["myFile"]["tmp_name"],$filename);
-        array_push($show,$dir.$_FILES["myFile"]["name"]);
-        return $show;//1
+        array_push($show,$dir.$_FILES["myFile"]["name"]);//上传后的文件名
+        return $show;
     }
 }
