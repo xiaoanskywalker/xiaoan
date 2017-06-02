96dce310244a0e6ee9e62aaf84e94c57861d9779
xasq <zqvvvv@126.com>
2017-06-02 17:59:51 +0800

    myhome

--- user/myhome.php
+++ user/myhome.php
@@ -4,13 +4,18 @@
 if ((file_exists("../common/config.php")) == false) {
     header("location:../install/");
 }
+/*基础参数赋值*/
+$baseurl = '..';
+$body = 'myhome.partial.php';
+$page['header']['title'] = '个人中心';
+$page['body']['class'] = 'index';
+
+
+/*引入初始文件*/
+require_once '../common/includes/common.php';
 /*引入Model类*/
-require_once '../common/conn.php';
-require_once '../model/Site.php';
-require_once '../model/User.php';
 require_once '../model/Home.php';
-/*获取站点信息*/
-$site = Site::get();
+
 /*判断用户是否登录*/
 session_start();
 if ($_SESSION["user"] != null) {
@@ -19,29 +24,11 @@ if ($_SESSION["user"] != null) {
     header("location:../");
 }
 /*参数及数组赋值*/
-$baseurl = '..';
-$body = 'myhome.partial.php';
-$page = array();
-$page['header'] = array();
-$page['header']['title'] = '个人中心';
-$page['message'] = array();
-$page['message']['accept'] = array();
-$page['message']['error'] = array();
-$page['body'] = array();
-$page['body']['class'] = 'index';//获取action参数
-$page['body']['nunber'] = 0;
+$action = array("index","avatar","myinfo","mytopic","message","changepwd");
 $page['body']['action'] = @$_REQUEST["action"];
 /*判断action参数是否合法*/
-$page['action'] = array("index","avatar","myinfo","mytopic","message","changepwd");
-$arrlength=count($page['action']);
-for($x=0;$x<$arrlength;$x++){
-    if ($page['body']['action'] == $page['action'][$x]){
-        $page['body']['nunber']=1;
-    }
-}
-/*若参数非法则统一为index参数*/
-if ($page['body']['nunber'] == 0){
-    $page['body']['action']='index';
+if (!(in_array($page['body']['action'] , $action))){
+    $page['body']['action'] = 'index';
 }
 /*头像上传操作*/
 if (!empty($_POST['avatar'])) {
